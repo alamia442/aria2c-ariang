ac6230b2eade9f92eeee4948071a1e45caf9072c
alamia442 <61982500+alamia442@users.noreply.github.com>
2021-04-13 19:07:02 +0700

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -1,68 +1,89 @@
-#!/bin/bash
-# Aria2 Autoupload.sh by P3TERX, adapt Heroku-AriaNG by xinxin8816 and Kwok1am.
+#!/usr/bin/env bash
+#
+# Copyright (c) 2018-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/aria2.conf
+# File name：upload.sh
+# Description: Use Rclone to upload files after Aria2 download is complete
+# Version: 2.2
+#
 
-## 文件过滤 File Filter ##
+## 基础设置 ##
+
+# Aria2 下载目录
+DOWNLOAD_PATH='/app/downloads'
+
+# Rclone 配置时填写的网盘名
+DRIVE_NAME="$RCLONE_DESTINATION"
+
+# 网盘目录。即上传目标路径，留空为网盘根目录，末尾不要有斜杠。
+DRIVE_PATH=''
+
+# 日志保存路径。注释或留空为不保存。
+#LOG_PATH='/root/.aria2/upload.log'
+
+## 文件过滤 ##
 
 # 限制最低上传大小，仅 BT 多文件下载时有效，用于过滤无用文件。低于此大小的文件将被删除，不会上传。
-# Limit the minimum upload size, which is only valid when downloading multiple BT files, and is used to filter useless files. Files below this size will be deleted and will not be uploaded.
 #MIN_SIZE=10m
 
 # 保留文件类型，仅 BT 多文件下载时有效，用于过滤无用文件。其它文件将被删除，不会上传。
-# Keep the file type, only effective when downloading multiple BT files, used to filter useless files. Other files will be deleted and will not be uploaded.
 #INCLUDE_FILE='mp4,mkv,rmvb,mov'
 
 # 排除文件类型，仅 BT 多文件下载时有效，用于过滤无用文件。排除的文件将被删除，不会上传。
-# Exclude file types, valid only when downloading multiple BT files, used to filter useless files. Excluded files will be deleted and will not be uploaded.
 #EXCLUDE_FILE='html,url,lnk,txt,jpg,png'
 
-## 高级设置 advanced settings ##
+## 高级设置 ##
 
 # RCLONE 配置文件路径
-# RCLONE Configuration file path
-export RCLONE_CONFIG=/app/rclone.conf
+#export RCLONE_CONFIG=$HOME/.config/rclone/rclone.conf
+
+# RCLONE 配置文件密码
+#export RCLONE_CONFIG_PASS=password
 
 # RCLONE 并行上传文件数，仅对单个任务有效。
-# RCLONE The number of files uploaded in parallel is only valid for a single task.
 #export RCLONE_TRANSFERS=4
 
 # RCLONE 块的大小，默认5M，理论上是越大上传速度越快，同时占用内存也越多。如果设置得太大，可能会导致进程中断。
-# RCLONE The size of the block, the default is 5M. Theoretically, the larger the upload speed, the faster it will occupy more memory. If the setting is too large, the process may be interrupted.
-export RCLONE_CACHE_CHUNK_SIZE=3M
+#export RCLONE_CACHE_CHUNK_SIZE=5M
 
 # RCLONE 块可以在本地磁盘上占用的总大小，默认10G。
-# RCLONE The total size that the block can occupy on the local disk, the default is 10G.
 #export RCLONE_CACHE_CHUNK_TOTAL_SIZE=10G
 
 # RCLONE 上传失败重试次数，默认 3
-# RCLONE Upload failed retry count, the default is 3
 #export RCLONE_RETRIES=3
 
 # RCLONE 上传失败重试等待时间，默认禁用，单位 s, m, h
-# RCLONE Upload failure retry wait time, the default is disabled, unit s, m, h
-export RCLONE_RETRIES_SLEEP=30s
+export RCLONE_RETRIES_SLEEP=10s
 
 # RCLONE 异常退出重试次数
-# RCLONE Abnormal exit retry count
 RETRY_NUM=3
 
 #============================================================
 
-DOWNLOAD_PATH='/app/downloads'
-FILE_PATH=$3                                          # Aria2传递给脚本的文件路径。BT下载有多个文件时该值为文件夹内第一个文件，如/root/Download/a/b/1.mp4
-REMOVE_DOWNLOAD_PATH=${FILE_PATH#${DOWNLOAD_PATH}/}   # 路径转换，去掉开头的下载路径。
-TOP_PATH=${DOWNLOAD_PATH}/${REMOVE_DOWNLOAD_PATH%%/*} # 路径转换，BT下载文件夹时为顶层文件夹路径，普通单文件下载时与文件路径相同。
-INFO="[INFO]"
-ERROR="[ERROR]"
-WARRING="[WARNING]"
+FILE_PATH=$3                                   # Aria2传递给脚本的文件路径。BT下载有多个文件时该值为文件夹内第一个文件，如/root/Download/a/b/1.mp4
+RELATIVE_PATH=${FILE_PATH#${DOWNLOAD_PATH}/}   # 路径转换，去掉开头的下载路径。
+TOP_PATH=${DOWNLOAD_PATH}/${RELATIVE_PATH%%/*} # 路径转换，BT下载文件夹时为顶层文件夹路径，普通单文件下载时与文件路径相同。
+RED_FONT_PREFIX="\033[31m"
+LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
+YELLOW_FONT_PREFIX="\033[1;33m"
+LIGHT_PURPLE_FONT_PREFIX="\033[1;35m"
+FONT_COLOR_SUFFIX="\033[0m"
+INFO="[${LIGHT_GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
+ERROR="[${RED_FONT_PREFIX}ERROR${FONT_COLOR_SUFFIX}]"
+WARRING="[${YELLOW_FONT_PREFIX}WARRING${FONT_COLOR_SUFFIX}]"
 
 TASK_INFO() {
     echo -e "
--------------------------- [TASK INFO] --------------------------
-Download path: ${DOWNLOAD_PATH}
-File path: ${FILE_PATH}
-Upload path: ${UPLOAD_PATH}
-Remote path: ${REMOTE_PATH}
--------------------------- [TASK INFO] --------------------------
+-------------------------- [${YELLOW_FONT_PREFIX}TASK INFO${FONT_COLOR_SUFFIX}] --------------------------
+${LIGHT_PURPLE_FONT_PREFIX}Download path:${FONT_COLOR_SUFFIX} ${DOWNLOAD_PATH}
+${LIGHT_PURPLE_FONT_PREFIX}File path:${FONT_COLOR_SUFFIX} ${FILE_PATH}
+${LIGHT_PURPLE_FONT_PREFIX}Upload path:${FONT_COLOR_SUFFIX} ${UPLOAD_PATH}
+${LIGHT_PURPLE_FONT_PREFIX}Remote path:${FONT_COLOR_SUFFIX} ${REMOTE_PATH}
+-------------------------- [${YELLOW_FONT_PREFIX}TASK INFO${FONT_COLOR_SUFFIX}] --------------------------
 "
 }
 
@@ -75,32 +96,31 @@ CLEAN_UP() {
 
 UPLOAD_FILE() {
     RETRY=0
-	echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
     while [ ${RETRY} -le ${RETRY_NUM} ]; do
         [ ${RETRY} != 0 ] && (
             echo
-            echo -e "$(date +"%m/%d %H:%M:%S") ${ERROR} ${UPLOAD_PATH} Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
+            echo -e "$(date +"%m/%d %H:%M:%S") ${ERROR} Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
             echo
         )
-        rclone copy -v "${UPLOAD_PATH}" "${REMOTE_PATH}"
+        rclone move -v "${UPLOAD_PATH}" "${REMOTE_PATH}"
         RCLONE_EXIT_CODE=$?
         if [ ${RCLONE_EXIT_CODE} -eq 0 ]; then
             [ -e "${DOT_ARIA2_FILE}" ] && rm -vf "${DOT_ARIA2_FILE}"
             rclone rmdirs -v "${DOWNLOAD_PATH}" --leave-root
-            echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Upload done: ${UPLOAD_PATH}"
-			rclone delete -v "${UPLOAD_PATH}"
+            echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Upload done: ${UPLOAD_PATH} -> ${REMOTE_PATH}"
+            [ $LOG_PATH ] && echo -e "$(date +"%m/%d %H:%M:%S") [INFO] Upload done: ${UPLOAD_PATH} -> ${REMOTE_PATH}" >>${LOG_PATH}
             break
         else
             RETRY=$((${RETRY} + 1))
             [ ${RETRY} -gt ${RETRY_NUM} ] && (
                 echo
                 echo -e "$(date +"%m/%d %H:%M:%S") ${ERROR} Upload failed: ${UPLOAD_PATH}"
+                [ $LOG_PATH ] && echo -e "$(date +"%m/%d %H:%M:%S") [ERROR] Upload failed: ${UPLOAD_PATH}" >>${LOG_PATH}
                 echo
             )
             sleep 3
         fi
     done
-	echo "$(($(cat numUpload)-1))" > numUpload # Minus 1
 }
 
 UPLOAD() {
@@ -125,18 +145,18 @@ fi
 
 if [ "${TOP_PATH}" = "${FILE_PATH}" ] && [ $2 -eq 1 ]; then # 普通单文件下载，移动文件到设定的网盘文件夹。
     UPLOAD_PATH="${FILE_PATH}"
-    REMOTE_PATH="${RCLONE_DESTINATION}/"
+    REMOTE_PATH="${DRIVE_NAME}:${DRIVE_PATH}"
     UPLOAD
     exit 0
 elif [ "${TOP_PATH}" != "${FILE_PATH}" ] && [ $2 -gt 1 ]; then # BT下载（文件夹内文件数大于1），移动整个文件夹到设定的网盘文件夹。
     UPLOAD_PATH="${TOP_PATH}"
-    REMOTE_PATH="${RCLONE_DESTINATION}/${REMOVE_DOWNLOAD_PATH%%/*}"
+    REMOTE_PATH="${DRIVE_NAME}:${DRIVE_PATH}/${RELATIVE_PATH%%/*}"
     CLEAN_UP
     UPLOAD
     exit 0
 elif [ "${TOP_PATH}" != "${FILE_PATH}" ] && [ $2 -eq 1 ]; then # 第三方度盘工具下载（子文件夹或多级目录等情况下的单文件下载）、BT下载（文件夹内文件数等于1），移动文件到设定的网盘文件夹下的相同路径文件夹。
     UPLOAD_PATH="${FILE_PATH}"
-    REMOTE_PATH="${RCLONE_DESTINATION}/${REMOVE_DOWNLOAD_PATH%/*}"
+    REMOTE_PATH="${DRIVE_NAME}:${DRIVE_PATH}/${RELATIVE_PATH%/*}"
     UPLOAD
     exit 0
 fi
