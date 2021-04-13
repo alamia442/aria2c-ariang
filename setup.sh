76cac2954dd950c96a0a3fa4ebd141940f421fc4
alamia442 <61982500+alamia442@users.noreply.github.com>
2021-04-13 16:37:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,15 @@
 # Install rclone static binary
 wget -q https://downloads.rclone.org/v1.53.0/rclone-v1.53.0-linux-amd64.zip
 unzip -q rclone-v1.53.0-linux-amd64.zip
-export PATH=$PWD/rclone-v1.53.0-linux-amd64:$PATH
+export PATH=/app/rclone-v1.53.0-linux-amd64:$PATH
 
 # Install aria2c static binary
 wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
 tar xf aria2-1.35.0-static-linux-amd64.tar.gz
-export PATH=$PWD:$PATH
+export PATH=/app:$PATH
 
 # Create download folder
-mkdir -p downloads
+mkdir -p /app/downloads
 
 # DHT
 wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
@@ -19,6 +19,6 @@ wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 
 # Tracker
 tracker_list=`curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat`
-echo "bt-tracker=$tracker_list" >> aria2c.conf
+echo "bt-tracker=$tracker_list" >> /app/aria2c.conf
 
 echo $PATH > PATH
